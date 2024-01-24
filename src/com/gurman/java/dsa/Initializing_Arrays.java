31a6f6c5614d8d40658c0fb777fe8097753d22c2
Ravneet Singh Brar <Rippybrar1994@gmail.com>
2024-01-24 14:57:33 -0500

    initial commit

---
+++ src/com/gurman/java/dsa/Initializing_Arrays.java
@@ -0,0 +1,12 @@
+package com.gurman.java.dsa;
+
+public class Initializing_Arrays {
+
+	public static void main(String[] args) {
+		int[] intArrayA = new int[5];
+
+		int[] intArrayB = { 2, 3, 4, 6, 78 };
+
+		System.out.println(intArrayA + "-----" + intArrayB);
+	}
+}
